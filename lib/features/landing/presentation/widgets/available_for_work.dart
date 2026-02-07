13cb0844d2a1b9d288711621604a72fb5b336ac0
Mahadev <rhythmhead97@gmail.com>
2026-02-07 11:43:54 +0530

    Create available_for_work.dart

---
+++ lib/features/landing/presentation/widgets/available_for_work.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../../../../config/config.dart';
+import '../../../../style/colors.dart';
+import '../../../../style/text_styles.dart';
+
+class AvailableForWork extends StatelessWidget {
+  const AvailableForWork({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return _body();
+  }
+
+  Container _body() {
+    return Container(
+      height: 45,
+      decoration: BoxDecoration(
+        color: const Color(0xff141415),
+        border: Border.all(
+          width: 0.1,
+          color: Colors.white.withOpacity(0.2),
+        ),
+        borderRadius: kDim.kRadius30,
+      ),
+      width: 180,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+          vertical: 10,
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // glowing circle
+              _glowingCircle(),
+              // label
+              _label(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// label
+  Text _label() {
+    return KStyles().med(
+      text: "Available For Work",
+      size: 14,
+      color: AppColors.kTextColor,
+    );
+  }
+
+  /// glowing Circle
+  Container _glowingCircle() {
+    return Container(
+      height: 8,
+      width: 8,
+      decoration: BoxDecoration(
+        color: const Color(0xff6dd33d),
+        borderRadius: kDim.kRadius100,
+      ),
+    );
+  }
+}
